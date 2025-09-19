dc3c5d728a2c8bf9a6f8627c37b45b5ecd4b687d
use <sathvaraneel52@gmail.com>
2025-09-20 01:16:38 +0530

    change folder name from conf to config

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import config from '../conf/config.js';
+import config from '../config/config.js';
 import databaseService from "../services/database.services.js";
 
 const axiosInstance = axios.create({
